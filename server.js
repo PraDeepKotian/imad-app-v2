d9ae120f6a4101654e27c87d5e409c758d0ded3e
PraDeepKotian <pradeepkotian1995@gmail.com>
2017-03-10 22:37:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var crypto = require('crypto');
+var bodyparser = require('body-parser');
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyparser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -27,6 +29,21 @@ app.get('/hash/:input',function(req,res){
    var hashstring = hash(req.params.input,'this-is-same-random-string');
    res.send(hashstring);
     
+});
+
+
+app.post('/create-user',function(req,res){
+    var username = req.body.username;
+    var pass = req.body.pass;
+    var salt = crypto.getRandomBytes(128).toString('hex');
+    var dbString = hash(password,salt);
+    pool.query('INSERT into "user"(username, pass)VALUES($1 ,$2)',[username , dbString], function(err,result){
+        if(err){
+            res.status(500).send(err.toSting());
+        }else{
+            res.send('user created');
+        }
+    } );
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
