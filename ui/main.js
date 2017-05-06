0e3e158e0b83517ce37733cbfce0b16e8308a213
PraDeep <pradeepkotian1995@gmail.com>
2017-05-06 10:05:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,12 @@
+//submit username password to login
+var submit = document.getElementById('submit_btn');
+submit.onclick = fumction(){
+    
+}
 
 
- var btn = document.getElementById('counter');
-            var counter = 0;
-            btn.onclick = function(){
-            counter = counter + 1;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString(); 
-            }
+
+
 /*
 var nameInput =document.getElementById('name');
 var name = nameInput.value;
